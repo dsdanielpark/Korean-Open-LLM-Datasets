126da3e2550a6cd2461c50db3fe679d0c387910f
parkminwoo <parkminwoo1991@gmail.com>
2023-07-15 01:21:17 +0900

    doc: add docstring

--- data/open_datasets/ko_shargpt_deepl_translate_cleaned_v1/data_gen.py
+++ data/open_datasets/ko_shargpt_deepl_translate_cleaned_v1/data_gen.py
@@ -1,12 +1,24 @@
 import json
 import concurrent.futures
 
-def extract_values(json_data_1, json_data_2, translate_type):
+
+def extract_values(json_data_1: list, json_data_2: list, translate_type: str):
+    """
+    Extracts values from JSON data for translation.
+
+    Args:
+        json_data_1 (list): A list of JSON data.
+        json_data_2 (list): A list of JSON data.
+        translate_type (str): The type of translation. It can be 'ko_to_en' for translating from Korean to English
+            or 'en_to_ko' for translating from English to Korean.
+
+    Returns:
+        list: A list of dictionaries containing instructions, input values, and output values for translation.
+    """
     values_list = []
     
-    if translate_type =='ko_to_en':
+    if translate_type == 'ko_to_en':
         for item_1, item_2 in zip(json_data_1, json_data_2):
-            
             conversations_1 = item_1.get("conversations", [])
             conversations_2 = item_2.get("conversations", [])
             
@@ -25,9 +37,8 @@ def extract_values(json_data_1, json_data_2, translate_type):
                 
                 if value_1 and value_2:
                     values_list.append(temp_dict)
-    elif translate_type =='en_to_ko':
+    elif translate_type == 'en_to_ko':
         for item_1, item_2 in zip(json_data_1, json_data_2):
-            
             conversations_1 = item_1.get("conversations", [])
             conversations_2 = item_2.get("conversations", [])
             
@@ -48,10 +59,20 @@ def extract_values(json_data_1, json_data_2, translate_type):
                     values_list.append(temp_dict)
     
     return values_list
-        
-    return values_list
 
-def process_json_files(file_path_1, file_path_2, chunk_size, output_filepath, translate_type):
+
+def process_json_files(file_path_1: str, file_path_2: str, chunk_size: int, output_filepath: str, translate_type: str):
+    """
+    Processes JSON files for translation and saves the results to an output file.
+
+    Args:
+        file_path_1 (str): The path to the first JSON file.
+        file_path_2 (str): The path to the second JSON file.
+        chunk_size (int): The size of each chunk for parallel processing.
+        output_filepath (str): The path to the output file.
+        translate_type (str): The type of translation. It can be 'ko_to_en' for translating from Korean to English
+            or 'en_to_ko' for translating from English to Korean.
+    """
     with open(file_path_1) as file:
         data_1 = json.load(file)[:200]
 
@@ -72,8 +93,7 @@ def process_json_files(file_path_1, file_path_2, chunk_size, output_filepath, tr
             values.extend(future.result())
 
     with open(output_filepath, "w", encoding="utf-8") as file:
-        json.dump(values, file, ensure_ascii=False,  indent=4, separators=(',', ':'))  
-
+        json.dump(values, file, ensure_ascii=False, indent=4, separators=(',', ':'))
 
 
 if __name__=="__main__":
